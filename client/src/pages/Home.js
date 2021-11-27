6c6447ee9848cc7069e14a555ace8fccece10d3b
Bora Kim <zh2464@naver.com>
2021-11-27 15:22:43 +0900

    [client] feat: #28 Add Home banner

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,11 +1,20 @@
 import React from "react";
 import Header from "../components/public/Header";
+import "../style/Home.scss";
 
 function Home(props) {
   return (
     <>
       <Header />
-      <div>{/* 헤더 밑으로 홈 */}</div>
+      <div className="banner">
+        <div className="wrapper">
+          <span>
+            넷플릭스 한달 요금으로
+            <br />
+            왓챠, 웨이브, 디즈니플러스까지
+          </span>
+        </div>
+      </div>
     </>
   );
 }

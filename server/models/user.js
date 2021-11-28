62d4e26c5c5c4fa5078274f17b58c427663cfca7
westtrain <it.joshualee@gmail.com>
2021-11-28 10:41:52 +0900

    [server] feat: #16 Add party, user Models

---
+++ server/models/user.js
@@ -0,0 +1,54 @@
+"use strict";
+const { Model } = require("sequelize");
+
+module.exports = (sequelize, DataTypes) => {
+  class User extends Model {
+    static associate(models) {
+      models.User.belongsToMany(models.Party, {
+        through: "User_party",
+        foreignKey: "user_id",
+      });
+    }
+    models.User.belongsTo(models.Statement, {
+        foreignKey: "user_id",
+        targetKey: "id",
+        onDelete: "cascade",
+      });
+    models.User.belongsTo(models.Payment, {
+        foreignKey: "user_id",
+        targetKey: "id",
+        onDelete: "cascade",
+      });
+  }
+  User.init(
+    {
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      socialType: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      money: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      deposit: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+    },
+    {
+      sequelize,
+      modelName: "User",
+      tableName: "Users",
+    }
+  );
+
+  return User;
+};

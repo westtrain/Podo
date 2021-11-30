bf30a07a4a6b7dbf798eb6972d855ea2ca357231
westtrain <it.joshualee@gmail.com>
2021-11-30 12:58:56 +0900

    [server] fix: typo

--- server/controller/tokenFunction/index.js
+++ server/controller/tokenFunction/index.js
@@ -16,7 +16,7 @@ module.exports = {
         secure: true,
         httpOnly: true,
         expires: new Date(Date.now() + 1000 * 60 * 60 * 48),
-        domain: ".keyplus.kr",
+        domain: ".podorang.com",
       })
       .json({ data: { accessToken }, message: "OK" });
   },

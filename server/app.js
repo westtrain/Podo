01152220f34c620c544ce018349940ba65071e9e
westtrain <it.joshualee@gmail.com>
2021-11-30 18:47:15 +0900

    [server] fix: codestates#14 change server success message

--- server/app.js
+++ server/app.js
@@ -32,7 +32,7 @@ app.use(cookieParser());
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-  res.send("Podo!");
+  res.send("Let's Podo!");
 });
 app.use("/auth", authRouter);
 app.use("/ott", ottRouter);

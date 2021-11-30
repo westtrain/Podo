07672ea1f3780e5f61c2cd7945fc638ac2674ae8
cherishxyun <81494787+cherishxyun@users.noreply.github.com>
2021-11-30 17:14:36 +0900

    Update ott.js

--- server/router/ott.js
+++ server/router/ott.js
@@ -3,6 +3,6 @@ const ottController = require("../controller/ott");
 
 const router = express.Router();
 
-router.get("/", ottController.getAllOTT); // 모든 태그 조회
+router.get("/", ottController.getAllOTT); // 모든 OTT 조회
 
 module.exports = router;

a2039c4041b1467b4e0305d5282d2f17c906560b
westtrain <it.joshualee@gmail.com>
2021-11-30 18:26:05 +0900

    [server] fix: change server message

--- server/app.js
+++ server/app.js
@@ -32,7 +32,7 @@ app.use(cookieParser());
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-  res.send("Let's Podo!");
+  res.send("Podo!");
 });
 app.use("/auth", authRouter);
 app.use("/ott", ottRouter);

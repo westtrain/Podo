6bfac205170a9e74791f56b7533d30f92a81767e
seungyun <cherishxyun@gmail.com>
2021-12-01 11:38:27 +0900

    [server] feat: #20 Controller and Router for User Code getUser and deleteUser `updateMoney` will be coded after programming `Payment` part.

--- server/controller/user.js
+++ server/controller/user.js
@@ -3,7 +3,7 @@ const { User } = require("../models");
 module.exports = {
   getUser: async (req, res) => {
     // 유저 상세 정보 조회
-    console.log(req);
+    // console.log(req.userId);
     const userInfo = await User.findOne({
       attributes: ["id", "name", "email", "socialType", "money", "deposit"],
       where: { id: req.userId },
@@ -26,7 +26,18 @@ module.exports = {
   },
   deleteUser: async (req, res) => {
     try {
-    } catch (error) {
+      const userInfo = await User.findOne({
+        attributes: ["id", "name", "email", "socialType", "money", "deposit"],
+        where: { id: req.userId },
+        raw: true,
+      });
+      if (userInfo) {
+        await User.destroy({ where: { id: req.userId } });
+        return res.status(200).json({ message: "Success" });
+      }
+      return res.status(401).json({ message: "Unauthorized request" });
+    } catch (err) {
+      console.log(err);
       return res.status(500).json({ message: "Server Error" });
     }
   },

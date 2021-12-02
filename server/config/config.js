5f64d9708453640e18e0c564a09523bcc80f6f4d
westtrain <it.joshualee@gmail.com>
2021-12-02 22:21:52 +0900

    [server] fix: codestates#17 Modify env setting

--- server/config/config.js
+++ server/config/config.js
@@ -1,12 +1,6 @@
 const dotenv = require("dotenv");
 require("dotenv").config();
-/*
-const userName = require("DATABASE_USERNAME");
-const password = require("DATABASE_PASSWORD");
-const database = require("DATABASE_NAME");
-const host = require("DATABASE_HOST");
-const port = require("DATABASE_PORT");
-*/
+
 module.exports = {
   port: {
     port: process.env.PORT,
@@ -19,15 +13,7 @@ module.exports = {
     port: process.env.DATABASE_PORT,
     dialect: "mysql",
   },
-  // development: {
-  //   username: userName,
-  //   password: password,
-  //   database: database,
-  //   host: host,
-  //   port: port,
-  //   dialect: "mysql",
-  //   timezone: "+09:00",
-  // },
+  
   development: {
     username: process.env.DATABASE_USERNAME,
     password: process.env.DATABASE_PASSWORD,

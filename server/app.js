5dfe11eebac15dff753b4d1f83bec8508af1bd10
westtrain <it.joshualee@gmail.com>
2021-12-07 22:02:10 +0900

    [server] fix: #127 Modify cors for send cookie

--- server/app.js
+++ server/app.js
@@ -20,7 +20,7 @@ const app = express();
 const port = config[process.env.NODE_ENV || "port"].port;
 
 const corsOption = {
-  origin: true,
+  origin: ["https://podorang.com"],
   optionsSuccessStatus: 200,
   credentials: true,
 };

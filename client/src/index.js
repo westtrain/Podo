f22659afccea957e06df3ad3d5aafbc144c55b78
Bora Kim <zh2464@naver.com>
2021-12-07 22:39:16 +0900

    [client] feat: #152 Add Stroe

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { persistStore } from "redux-persist";
+import { PersistGate } from "redux-persist/integration/react";
 import "./index.css";
+import store from "./redux/configureStore";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
+const persistor = persistStore(store);
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );

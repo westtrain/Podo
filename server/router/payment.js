8944eaed0d2142e9b8d1e69ac171c555c7c8cb67
cherishxyun <81494787+cherishxyun@users.noreply.github.com>
2021-12-08 14:43:52 +0900

    [server] fix: #160 Delete checkAccount router

--- server/router/payment.js
+++ server/router/payment.js
@@ -1,8 +1,6 @@
 const express = require("express");
 const paymentController = require("../controller/payment");
-const accountController = require("../controller/importFunction/account");
 const isAuth = require("../middleware/verifyToken");
-
 const router = express.Router();
 
 router.get("/", isAuth, paymentController.getUsersPaymentInfo); // user 결제 정보 조회
@@ -12,6 +10,5 @@ router.patch("/settlement", isAuth, paymentController.changeSettlement); // 정�
 router.post("/credit", isAuth, paymentController.enrollCard); // 카드 정보 등록
 router.post("/account", isAuth, paymentController.enrollAccount); // 인출 계좌 등록
 router.post("/settlement", isAuth, paymentController.enrollSettlement); // 정산일 등록
-router.get("/checkAccount", accountController.checkAccount); // user 결제 정보 조회
 
 module.exports = router;

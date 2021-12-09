802d8af37b75342eea5f47625a88457eddd1eef7
seungyun <cherishxyun@gmail.com>
2021-12-09 17:47:55 +0900

    [server] feat: #20 Deal with edge case Deal with edge case if an user requests to delete ones own account without leaving all parties

--- server/controller/user.js
+++ server/controller/user.js
@@ -1,4 +1,5 @@
-const { User } = require("../models");
+const { User, Party, Payment, Statement } = require("../models");
+const db = require("../models");
 
 module.exports = {
   getUser: async (req, res) => {
@@ -17,20 +18,21 @@ module.exports = {
       return res.status(500).json({ message: "Server Error" });
     }
   },
+
   updateMoney: async (req, res) => {
     // 토스API 이용하여 포도머니를 계좌로 인출
     try {
       const user_id = req.userId;
       const withdraw = req.query.withdraw;
       if (user_id) {
-        const userMoney = await User.findOne({
+        let userInfo = await User.findOne({
           attributes: ["money"],
           where: { id: user_id },
           raw: true,
         });
-        const result = userMoney.money - withdraw;
+        const result = userInfo.money - withdraw;
         await User.update({ money: result }, { where: { id: user_id } });
-        const userInfo = await User.findOne({
+        userInfo = await User.findOne({
           attributes: ["id", "name", "email", "socialType", "money", "deposit"],
           where: { id: user_id },
           raw: true,
@@ -42,7 +44,9 @@ module.exports = {
       return res.status(500).json({ message: "Server Error" });
     }
   },
+
   deleteUser: async (req, res) => {
+    const user_id = req.userId;
     try {
       //해당 아이디를 가진 유저 조회
       const userInfo = await User.findOne({
@@ -50,9 +54,23 @@ module.exports = {
         where: { id: req.userId },
         raw: true,
       });
+
+      // 모든 파티 탈퇴했는지 여부 조회
+      const userPartyInfo = await db.sequelize.models.User_party.findAll({
+        where: { user_id },
+      });
+      if (userPartyInfo.length !== 0) {
+        return res.status(422).json({ message: "Have to leave all parties you joined" });
+      }
       //해당 유저가 있다면 데이터 삭제
       if (userInfo) {
-        await User.destroy({ where: { id: req.userId } });
+        User.destroy({ where: { id: user_id } });
+        Payment.destroy({ where: { user_id } });
+        Statement.destroy({ where: { user_id } });
+        //Party member 삭제
+        db.sequelize.models.User_party.destroy({
+          where: { user_id },
+        });
         return res.status(200).json({ message: "Success" });
       }
       return res.status(401).json({ message: "Unauthorized request" });

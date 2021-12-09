6a1acb2f38df03d4e3b5bde1dd87f23fa1eafb92
Bora Kim <zh2464@naver.com>
2021-12-10 00:25:45 +0900

    [client] feat: #112 Fix favicon

--- client/src/components/public/LoginCallBack.js
+++ client/src/components/public/LoginCallBack.js
@@ -1,12 +1,11 @@
 import React, { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { getUser } from "../../redux/API/userAPI";
 
 function LoginCallBack(props) {
   const navigate = useNavigate();
   const dispatch = useDispatch();
-  //const userState = useSelector((state) => state.user);
 
   useEffect(async () => {
     await dispatch(getUser());

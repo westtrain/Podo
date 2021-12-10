5bd8d468b0051004dabb2ee994e5bd3d49741cac
seungyun <cherishxyun@gmail.com>
2021-12-10 19:43:55 +0900

    [server] feat: #21 Deal with Edge Case Deal with edge case when an user has already

--- server/controller/party.js
+++ server/controller/party.js
@@ -3,17 +3,15 @@ const { Party } = require("../models");
 
 module.exports = {
   getUsersParty: async (req, res) => {
-    const userId = req.userId;
+    const user_id = req.userId;
     const usersParties = [];
-    // console.log(userId);
     try {
       const allUsersPartiesId = await db.sequelize.models.User_party.findAll({
         attributes: ["party_id"],
-        where: {
-          user_id: userId,
-        },
+        where: { user_id },
         raw: true,
       });
+      console.log(allUsersPartiesId);
       for (let i = 0; i < allUsersPartiesId.length; i++) {
         const usersParty = await Party.findOne({
           where: {
@@ -21,7 +19,7 @@ module.exports = {
           },
           raw: true,
         });
-        // console.log(usersParty);
+        console.log(usersParty);
         usersParties[i] = usersParty;
       }
 
@@ -35,10 +33,11 @@ module.exports = {
   },
 
   getParty: async (req, res) => {
-    const partyId = req.params.party_id;
+    const party_id = req.params.id;
+    console.log(party_id);
     try {
       const partyInfo = await Party.findOne({
-        where: { id: partyId },
+        where: { id: party_id },
         raw: true,
       });
       if (!partyInfo) {
@@ -67,7 +66,7 @@ module.exports = {
 
   getFilteredParties: async (req, res) => {
     // 0. 쿼리에서 받은 정보를 구조분해할당으로 각 변수에 담는다.
-    const ottId = req.query.ott_id;
+    const ott_id = req.query.ott_id;
     const filteredPartiesByDate = [];
     let date = null;
 
@@ -78,9 +77,7 @@ module.exports = {
     try {
       // 1. ottId로 조회해서 filteredPartiesByOttId에 담는다
       const filteredPartiesByOttId = await Party.findAll({
-        where: {
-          ott_id: ottId,
-        },
+        where: { ott_id },
         raw: true,
       });
       // 2. 특정 날짜가 있다면 날짜와 같은 정보만 담는다.
@@ -121,7 +118,7 @@ module.exports = {
         start_date &&
         end_date
       ) {
-        // 이미 create나 join한 같은 ott종류의 파티가 있다면 create 방지. ex) 넷플 파티에 이미 create/join 했는데 또 넷플 파티 만들려고 하는 경우
+        // 같은 ott종류의 이미 create나 join한 파티가 있다면 create 방지. ex) 넷플 파티에 이미 create/join 했는데 또 넷플 파티 만들려고 하는 경우
 
         // 1차 필터링: Party 테이블에서 create하려는 ott를 공유하고 있는 파티 조회
         const ottParty = await Party.findAll({ where: { ott_id } });
@@ -148,6 +145,7 @@ module.exports = {
               .json({ message: "You already created or joined the same party" });
           }
         }
+
         // 2. 각 변수에 담기 값을 알맞는 필드에 넣고 업데이트한다.
         await Party.create({
           ott_id,
@@ -232,20 +230,48 @@ module.exports = {
 
   joinParty: async (req, res) => {
     // 0. 쿠키를 통해 받아온 토큰으로 유저 아이디를 만든다.
-    const userId = req.userId;
+    const user_id = req.userId;
     // 1. 바디에서 받은 정보를 구조분해할당으로 각 변수에 담는다.
     const { party_id } = req.body;
     try {
       // 2. 유저가 이 파티에 맴버로 있는지 확인
       const isMember = await db.sequelize.models.User_party.findAll({
-        where: {
-          party_id: party_id,
-          user_id: userId,
-        },
+        where: { party_id, user_id },
       }); //where 빠졌었음. await 넣어야 promise형태가 아닌 배열 형태의 결과값 나옴
       if (isMember.length !== 0) {
         //배열 형태에서 비어있음을 표현하기 위해서는 length 사용해야 함.
-        return res.status(422).json({ message: "already joined this party" });
+        return res.status(422).json({ message: "You already created or joined this party" });
+      }
+
+      // Edge Case: 같은 ott종류의 이미 create나 join한 파티가 있다면 create 방지. ex) 넷플 파티에 이미 create/join 했는데 또 넷플 파티 만들려고 하는 경우
+
+      // 1차 필터링: Party 테이블에서 create하려는 ott를 공유하고 있는 파티 조회
+      const ott = await Party.findOne({ where: { id: party_id } });
+      console.log("ott======================", ott);
+      const ottParty = await Party.findAll({ where: { ott_id: ott.dataValues.ott_id } });
+      console.log("ottParty================", ottParty);
+
+      // 2차 필터링: 1개 이상 있다면 map을 사용하여 각 파티에 '현재 create하려는 유저'가 있는지 확인
+      if (ottParty.length !== 0) {
+        //alreadyJoined는 실행되지 않은 Promise로 Promise.all을 사용하지 않으면 alreadyJoinedArr는 Promise 배열이 나오게 됨.
+        let alreadyJoinedArr = await Promise.all(
+          ottParty.map((party) => {
+            const alreadyJoined = db.sequelize.models.User_party.findAll({
+              where: { party_id: party.dataValues.id, user_id },
+            });
+            return alreadyJoined;
+          })
+        );
+        // map과 Promise.all 사용으로 인해 비어있어도 2차원배열('[[]]')로 나오기 때문에 flat 사용
+        alreadyJoinedArr = alreadyJoinedArr.flat();
+        console.log(alreadyJoinedArr);
+
+        //해당 유저가 있다면 422 에러
+        if (alreadyJoinedArr.length !== 0) {
+          return res.status(422).json({
+            message: "You already created or joined the party that shares the same kind of OTT",
+          });
+        }
       }
 
       // 3. party_id로 조회한다.
@@ -261,13 +287,10 @@ module.exports = {
         if (members.length === data.numbers_num) {
           return res.status(422).json({ meessage: "Party already full" });
         }
-        members += `,${userId}`;
+        members += `,${user_id}`;
         Party.update({ members }, { where: { id: party_id } });
         // 6. 새 유저와 파티관계를 조인테이블에 넣어준다
-        db.sequelize.models.User_party.create({
-          party_id: party_id,
-          user_id: userId,
-        });
+        db.sequelize.models.User_party.create({ party_id, user_id });
       });
       return res.status(200).json({ message: "Success" });
     } catch (error) {

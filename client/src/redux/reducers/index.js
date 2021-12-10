7fe1a8612def662c33a7882234fabe35dfffdfee
Bora Kim <zh2464@naver.com>
2021-12-10 17:35:48 +0900

    [client] feat: #208 Add axiosConfing

--- client/src/redux/reducers/index.js
+++ client/src/redux/reducers/index.js
@@ -4,6 +4,11 @@ import storage from "redux-persist/lib/storage"; // local storage에 저장
 import userReducer from "./userSlice";
 import partyReducer from "./partySlice";
 import modalReducer from "./modalSlice";
+import loadingReducer from "./loadingSlice";
+
+// 리덕스의 store는 페이지를 새로고침 할 경우 state가 사라질 우려가 있어
+// 이에 대응하기 위해 redux-persist를 사용
+// 아래는 local storage에 저장하는 방식
 
 const persistConfig = {
   key: "root",
@@ -16,5 +21,6 @@ export const persistedReducer = persistReducer(
     user: userReducer,
     party: partyReducer,
     modal: modalReducer,
+    loading: loadingReducer,
   })
 );

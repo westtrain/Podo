16599fe8598ac0d3d5074528decaea10046bfbaf
westtrain <it.joshualee@gmail.com>
2021-12-14 23:43:04 +0900

    [server] feat: #23 Add Check and insert credit card info by import api

--- server/router/payment.js
+++ server/router/payment.js
@@ -4,10 +4,8 @@ const isAuth = require("../middleware/verifyToken");
 const router = express.Router();
 
 router.get("/", isAuth, paymentController.getUsersPaymentInfo); // user 결제 정보 조회
-router.patch("/credit", isAuth, paymentController.changeCard); // 카드 변경
-router.patch("/settlement", isAuth, paymentController.changeSettlement); // 정산일 변경
-router.post("/credit", isAuth, paymentController.enrollCard); // 카드 정보 등록
+router.post("/credit", isAuth, paymentController.updateCard); // 카드 정보 등록
 router.post("/account", isAuth, paymentController.updateAccount); // 인출 계좌 등록
-router.post("/settlement", isAuth, paymentController.enrollSettlement); // 정산일 등록
+router.post("/settlement", isAuth, paymentController.updateSettlement); // 정산일 등록
 
 module.exports = router;

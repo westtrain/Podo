789f16d57efcd71dbbad00bbb29e5126b3af7385
westtrain <it.joshualee@gmail.com>
2021-12-20 21:56:10 +0900

    [server] feat: #27 Modify capital seed

--- server/seeders/capital.js
+++ server/seeders/capital.js
@@ -2,7 +2,7 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.bulkInsert("Capital", [{ id: 1, money: 100000 }], {});
+    await queryInterface.bulkInsert("Capital", [{ id: 1, money: null, total_amount: 1000000 }], {});
   },
 
   down: async (queryInterface, Sequelize) => {
